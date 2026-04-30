63bb4d8fd176d44d647fef16b455825480721ef7
MichalS <D00282723@student.dkit.ie>
2026-04-30 13:06:47 +0100

    feat: add hopper header

---
+++ Hopper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by micsa on 30/04/2026.
+//
+
+#include "Hopper.h"
---
+++ Hopper.h
@@ -0,0 +1,22 @@
+//
+// Created by micsa on 30/04/2026.
+//
+
+#ifndef BUGS_LIFE_HOPPER_H
+#define BUGS_LIFE_HOPPER_H
+
+#include "Bug.h"
+
+class Hopper : public Bug
+{
+    private:
+        int hopLength;
+
+    public:
+        Hopper(int id, int x, int y, int direction, int health, int hopLength);
+        void move() override;
+;
+};
+
+
+#endif //BUGS_LIFE_HOPPER_H
