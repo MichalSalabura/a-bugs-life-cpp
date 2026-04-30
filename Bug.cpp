08199e5cab5fda1fd4f9712acdd2cf5707d4aecb
MichalS <D00282723@student.dkit.ie>
2026-04-30 12:34:08 +0100

    feat: implement isWayBlocked

--- Bug.cpp
+++ Bug.cpp
@@ -6,4 +6,9 @@
 
 bool Bug::isWayBlocked()
 {
+    if (direction == 1 && position.second == 0) return true;
+    if (direction == 2 && position.first == 9) return true;
+    if (direction == 3 && position.second == 9) return true;
+    if (direction == 4 && position.first == 0) return true;
+    return false;
 }
