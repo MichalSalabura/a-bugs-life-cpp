86cb2d7b467756e1e63fd76732ece430060236f9
MichalS <D00282723@student.dkit.ie>
2026-04-30 13:20:18 +0100

    feat: implement Hopper move

--- Hopper.cpp
+++ Hopper.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Hopper.h"
+using namespace std;
 
 Hopper::Hopper(int id, int x, int y, int direction, int health, int hopLength)
 {
@@ -13,4 +14,31 @@ Hopper::Hopper(int id, int x, int y, int direction, int health, int hopLength)
     this->alive = true;
     this->hopLength = hopLength;
     this->path.push_back(this->position);
+}
+
+void Hopper::move()
+{
+    while (isWayBlocked())
+    {
+        direction = rand() % 4 + 1;
+    }
+
+    if (direction == 1)
+    {
+        position.second = (position.second - hopLength < 0) ? 0 : position.second - hopLength;
+    }
+    else if (direction == 2)
+    {
+        position.first = (position.first + hopLength > 9) ? 9 : position.first + hopLength;
+    }
+    else if (direction == 3)
+    {
+        position.second = (position.second + hopLength > 9) ? 9 : position.second + hopLength;
+    }
+    else
+    {
+        position.first = (position.first - hopLength < 0) ? 0 : position.first - hopLength;
+    }
+
+    path.push_back(this->position);
 }
