e7704b7fe56dfedf7fe03fea2632fa7101e523c9
MichalS <D00282723@student.dkit.ie>
2026-04-30 13:10:49 +0100

    feat: implement Hopper constructor

--- Hopper.cpp
+++ Hopper.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "Hopper.h"
+
+Hopper::Hopper(int id, int x, int y, int direction, int health, int hopLength)
+{
+    this->id = id;
+    this->position = {x, y};
+    this->direction = direction;
+    this->health = health;
+    this->alive = true;
+    this->hopLength = hopLength;
+    this->path.push_back(this->position);
+}
