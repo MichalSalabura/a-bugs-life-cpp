5797e50ca338071efd76fcc41f4d6a879eb38cc8
MichalS <D00282723@student.dkit.ie>
2026-05-05 00:35:52 +0100

    feat: implement main menu

--- main.cpp
+++ main.cpp
@@ -9,11 +9,39 @@ using namespace std;
 int main()
 {
     Board board;
-    board.loadBugs("bugs.txt");
-    // board.displayAllBugs();
-    // board.findBug();
-    // board.lifeHistory();
-    // board.exit();
-    // board.displayAllCells();
-    board.runSimulation();
+
+    int choice = 0;
+    while (choice != 8) {
+        cout << "\n1. Initialise Bug Board" << endl;
+        cout << "2. Display All Bugs" << endl;
+        cout << "3. Find a Bug" << endl;
+        cout << "4. Tap the Bug Board" << endl;
+        cout << "5. Display Life History" << endl;
+        cout << "6. Display All Cells" << endl;
+        cout << "7. Run Simulation" << endl;
+        cout << "8. Exit" << endl;
+        cout << "Choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            board.loadBugs("bugs.txt");
+        } else if (choice == 2) {
+            board.displayAllBugs();
+        } else if (choice == 3) {
+            board.findBug();
+        } else if (choice == 4) {
+            board.tapBoard();
+        } else if (choice == 5) {
+            board.lifeHistory();
+        } else if (choice == 6) {
+            board.displayAllCells();
+        } else if (choice == 7) {
+            board.runSimulation();
+        } else if (choice == 8) {
+            board.exit();
+        } else {
+            cout << "Invalid choice." << endl;
+        }
+    }
+    return 0;
 }
