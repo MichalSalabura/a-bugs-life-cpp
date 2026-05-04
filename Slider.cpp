1fba1c1941b52c06bfbfaee8d5e0aef6fd369319
MichalS <D00282723@student.dkit.ie>
2026-05-05 00:57:45 +0100

    feat: implement slider constructor and move

--- Slider.cpp
+++ Slider.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "Slider.h"
+#include <cstdlib>
+
+using namespace std;
+
+Slider::Slider(char t, int id, int x, int y, int direction, int health)
+{
+    this->type = t;
+    this->id = id;
+    this->position = {x, y};
+    this->direction = direction;
+    this->health = health;
+    this->alive = true;
+    this->path.push_back(this->position);
+}
+
+void Slider::move()
+{
+    int x = position.first;
+    int y = position.second;
+    int newX = x, newY = y;
+    int sideways = (rand() % 2 == 0) ? -1 : 1;
+
+    if (direction == 1)
+    {
+        newY = y - 2;
+        newX = x + sideways;
+    } else if (direction == 2)
+    {
+        newX = x + 2;
+        newY = y + sideways;
+    } else if (direction == 3)
+    {
+        newY = y + 2;
+        newX = x + sideways;
+    } else if (direction == 4)
+    {
+        newX = x - 2;
+        newY = y + sideways;
+    }
+
+    if (newX < 0 || newX > 9 || newY < 0 || newY > 9) {
+        direction = rand() % 4 + 1;
+        move();
+        return;
+    }
+
+    position = {newX, newY};
+    path.push_back(position);
+}
